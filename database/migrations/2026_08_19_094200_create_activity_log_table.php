f12cdcdd9351796be63ad8a171f95fdc4f6a4e89
muhammad Alnabky <muhammadalnabky00900@gmail.com>
2026-08-19 12:55:25 +0300

    Notification

--- database/migrations/2026_08_19_094200_create_activity_log_table.php
+++ database/migrations/2026_08_19_094200_create_activity_log_table.php
@@ -8,26 +8,20 @@ class CreateActivityLogTable extends Migration
 {
     public function up()
     {
-        Schema::create('activity_log', function (Blueprint $table) {
+        Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table) {
             $table->bigIncrements('id');
-
             $table->string('log_name')->nullable();
             $table->text('description');
-
-            $table->nullableMorphs('subject');
-            $table->nullableMorphs('causer');
-
+            $table->nullableMorphs('subject', 'subject');
+            $table->nullableMorphs('causer', 'causer');
             $table->json('properties')->nullable();
-            $table->string('event')->nullable();
-
             $table->timestamps();
-
             $table->index('log_name');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('activity_log');
+        Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
     }
 }
