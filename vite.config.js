abf0aaae2666f250ee9ac42a23fa642615a4ac08
muhammad Alnabky <muhammadalnabky00900@gmail.com>
2026-09-07 22:52:31 +0300

    ANNOUNCEMENT

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-
                 'Modules/Messagings/resources/assets/js/app.js',
+                'Modules/Announcement/resources/assets/js/app.js',
             ],
-
             refresh: true,
         }),
     ],
